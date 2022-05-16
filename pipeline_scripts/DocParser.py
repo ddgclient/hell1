e4d057356c1d78a0965be1b700a43aca23b0c22a
ddgclient <103461668+ddgclient@users.noreply.github.com>
2022-05-16 00:15:44 -0700

    Update DocParser.py

--- pipeline_scripts/DocParser.py
+++ pipeline_scripts/DocParser.py
@@ -103,8 +103,8 @@ def renameFile(filepath):
 def removeImageFolder(filepath):
     print("Starting removeImageFolder()", flush=True)
     tempFile = os.path.join(os.getcwd(),"temp.txt")
-    with open(tempFile, 'w', encoding="utf8") as fileOut:
-        with open(filepath, "r") as fileIn:
+    with open(tempFile, 'w') as fileOut:
+        with open(filepath, "r", encoding="cp437", errors='ignore') as fileIn:
             for line in fileIn:
                 rslt = re.search(r'(.*?<img\s*src=\")images\/(.*)', line)
                 if rslt:
